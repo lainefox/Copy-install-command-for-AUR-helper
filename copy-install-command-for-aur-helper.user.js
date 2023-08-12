bb7c7ef6d696b89fbfa8694ffa42a993a2f7a1ec
laine <lainefox@proton.me>
2023-08-12 23:55:31 +0300

    Added auto copy functionality

--- copy-install-command-for-aur-helper.user.js
+++ copy-install-command-for-aur-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Copy install command for AUR helper
 // @author      Bing AI, and my sleep deprived ass <- laine
-// @version     1.0.1
+// @version     1.0.6
 // @namespace   https://github.com/lainefox/Copy-install-command-for-AUR-helper
 // @grant       GM.setClipboard
 // @run-at      document-end
@@ -57,6 +57,67 @@ window.addEventListener("DOMContentLoaded", function() {
     
     container.appendChild(link);
     container.appendChild(select);
-    li.appendChild(container);
-    ul.insertBefore(li, ul.firstChild);
+    
+    // Create a new checkbox element for the Auto copy option
+    var autoCopyCheckbox = document.createElement("input");
+    autoCopyCheckbox.type = "checkbox";
+    
+    // Restore the Auto copy option from local storage
+    var autoCopyEnabled = localStorage.getItem("autoCopyEnabled") === "true";
+    autoCopyCheckbox.checked = autoCopyEnabled;
+    
+     // Automatically copy the command at page load if Auto copy is enabled
+     if (autoCopyEnabled) {
+         var selectedManager = select.value;
+         var command = selectedManager + " -S " + pkgName;
+         GM.setClipboard(command, "text");
+     }
+     
+     // Save the Auto copy option to local storage when the user clicks on the checkbox
+     autoCopyCheckbox.addEventListener("change", function() {
+         localStorage.setItem("autoCopyEnabled", autoCopyCheckbox.checked);
+         
+         if (autoCopyCheckbox.checked) {
+             // Automatically copy the command if Auto copy is enabled
+             var selectedManager = select.value;
+             var command = selectedManager + " -S " + pkgName;
+             GM.setClipboard(command, "text");
+         }
+     });
+     
+     // Create a new label element for the Auto copy option
+     var autoCopyLabel = document.createElement("label");
+     autoCopyLabel.textContent = "Copy automatically";
+  
+  	 // Add an event listener to the label element
+		 autoCopyLabel.addEventListener("click", function() {
+         // Toggle the checked state of the checkbox
+         autoCopyCheckbox.checked = !autoCopyCheckbox.checked;
+    
+         // Trigger the change event on the checkbox
+         var event = new Event("change");
+         autoCopyCheckbox.dispatchEvent(event);
+         });
+
+     
+     // Append the Auto copy checkbox and label to the container element
+     container.appendChild(document.createElement("br"));
+     container.appendChild(autoCopyCheckbox);
+     container.appendChild(autoCopyLabel);
+     
+     li.appendChild(container);
+     
+     ul.insertBefore(li, ul.firstChild);
+     
+     // Add some spacing at the end of the options added by this userscript
+     container.style.marginBottom = "12px";
+     
+     // Fix the checkbox label being bold by setting its font weight to normal
+     autoCopyLabel.style.fontWeight = "normal";
+  
+     // Get the computed value of the color property for the a selector
+	 var linkColor = getComputedStyle(document.querySelector("a")).color;
+
+     // Set the color of the label element
+	 autoCopyLabel.style.color = linkColor;
 }, false);
